87adcb1961598c923c12875be71b9af99a799821
AmanSN997 <amanjeetsinghnarula@gmail.com>
2025-03-17 15:56:03 +0530

    Update server.R

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ library(quantmod)
 library(tidyverse)
 library(lubridate)
 library(plotly)
+library(DT) # For data tables with conditional formatting
 
 server <- function(input, output) {
 
@@ -83,5 +84,32 @@ server <- function(input, output) {
         )
       p
     }
+  })
+
+  output$daily_changes_table <- renderDataTable({
+    all_changes <- lapply(nifty_symbols, function(symbol) {
+      tryCatch({
+        data <- getSymbols(symbol, from = Sys.Date() - 1, to = Sys.Date(), auto.assign = FALSE)
+        if (nrow(data) > 1) {
+          prev_close <- Cl(data)[1] # Previous day's closing price
+          curr_close <- Cl(data)[2] # Current day's closing price
+          change <- curr_close - prev_close
+          change_percent <- (change / prev_close) * 100
+          data.frame(Symbol = symbol, Change = change, ChangePercent = change_percent)
+        } else {
+          data.frame(Symbol = symbol, Change = NA, ChangePercent = NA)
+        }
+      }, error = function(e) {
+        data.frame(Symbol = symbol, Change = NA, ChangePercent = NA)
+      })
+    })
+
+    changes_df <- do.call(rbind, all_changes)
+
+    datatable(changes_df, options = list(pageLength = 50)) %>%
+      formatStyle(
+        'Change',
+        backgroundColor = styleInterval(0, c('red', 'lightgreen'))
+      )
   })
 }
